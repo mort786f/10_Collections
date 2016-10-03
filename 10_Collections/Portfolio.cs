6cf9f7622e05f3750aea81ddc7393688fb5e38c3
morte <morte@DESKTOP-AJQ2HRF>
2016-10-03 09:09:43 +0200

    Add project files.

---
+++ 10_Collections/Portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_Collections
+{
+    internal class Portfolio
+    {
+        public void AddAsset(Stock stockIBM)
+        {
+            
+        }
+
+        public IList<IAsset> GetAssets()
+        {
+            
+        }
+    }
+}
---
+++ 10_Collections/Stock.cs
@@ -0,0 +1,17 @@
+﻿namespace _10_Collections
+{
+    internal class Stock: IAsset
+    {
+        
+        public Stock(string symbol, double pricePerShare, int numShares)
+        {
+            Symbol = symbol;
+            PricePerShare = pricePerShare;
+            NumShare = numShares;
+        }
+        public string Symbol { get; set; }
+        public double PricePerShare { get; set; }
+        public int NumShare { get; set; }
+    }
+
+}
